0077029a08bb3cdf583ca2163773751533092991
두소원 <enthdnjs11@gmail.com>
2024-02-02 13:08:25 +0900

    feat: Task Entity 생성

--- Backend/src/main/java/com/savetheday/backend/db/entity/Task.java
+++ Backend/src/main/java/com/savetheday/backend/db/entity/Task.java
@@ -1,13 +1,52 @@
 package com.savetheday.backend.db.entity;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/task")
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="task")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long TaskId;
+
+//    @Co
+//    private Long userId;
+    @ManyToOne
+    @JoinColumn(name="user_id", insertable = false, updatable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    private String content;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @Column(nullable = false)
+    private LocalDate due_date;
+
+    @Column(nullable = false, length = 10)
+    private String status;
+
+    @Column(nullable = false, length = 10)
+    private String importance;
+
+    @Column(length = 10)
+    private String emotion;
+
 
 }
