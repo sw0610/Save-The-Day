9d81f062e1806314783b1e609c78c28f4f580d4a
sw0610 <enthdnjs11@gmail.com>
2024-02-03 17:23:53 +0900

    feat: 헤더 생성(메뉴바 제외)

---
+++ Frontend/todo-list/src/components/Task/TaskHeader.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div class="header" id="header">
+        <div id="titleBar">
+            <div id="menu">button</div>
+            <div id="title">Todo List</div>
+        </div>
+    </div>
+</template>
+<script>
+
+
+</script>
+<style scoped>
+    .header{
+        background-color: #FFBAB5;
+        padding-left: 32px;        
+        padding-right: 32px;
+    }
+
+    #header{
+        height: 120px;
+    }
+    #titleBar{
+        position: relative;
+    }
+
+    #menu{
+        /* position: absolute;
+        padding-top: 30px; */
+        /* padding-top: 30; */
+    
+
+    }
+
+    #title{
+        /* position: absolute; */
+        text-align: center;
+        padding-top: 30px;
+        font-size: 20px;
+        font-family: 'Noto Sans', sans-serif;
+        font-weight: 400;
+        color: #FFFFFF;
+        /* display: inline-block; */
+    }
+
+</style>
---
+++ Frontend/todo-list/src/views/TaskApp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="Task">
+        <Task-Header></Task-Header>
+        <Weekly-Calender></Weekly-Calender>
+        <Task-List></Task-List>
+    </div>
+    
+</template>
+
+<script>
+import TaskHeader from '../components/Task/TaskHeader.vue'
+import WeeklyCalender from '../components/Task/WeeklyCalendar.vue'
+import TaskList from '../components/Task/TaskList.vue'
+
+export default {
+    components: {
+        'Task-Header': TaskHeader,
+        'Weekly-Calender': WeeklyCalender,
+        'Task-List': TaskList
+    },
+}
+</script>
+<style scoped>
+    .Task{
+        margin: 0%;
+    }
+</style>
