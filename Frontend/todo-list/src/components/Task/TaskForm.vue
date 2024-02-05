26687d3990d87198e3db195a8f93a177ab6bfcc0
두소원 <enthdnjs11@gmail.com>
2024-02-05 17:39:47 +0900

    feat: detail 페이지 만드는 중

--- Frontend/todo-list/src/components/Task/TaskForm.vue
+++ Frontend/todo-list/src/components/Task/TaskForm.vue
@@ -1,25 +1,28 @@
 <template>
     <div id="form">
-        <div id="title">
-            <div>Title</div>
-            <input>
+        <div>
+            <div class="formComponent" id="taskTitle">
+                <div class="title">Title</div>
+                <input class="box">
+            </div>
+            <div class="formComponent" id="date">
+                <div class="title">Date</div>
+                <button class="box"></button>  
+            </div>
+            <div  class="formComponent" id="importance">
+                <div class="title">Importance</div>
+                <button class="box"></button>  
+            </div>
+            <div class="formComponent" id="status">
+                <div class="title">Status</div>
+                <button class="box"></button>  
+            </div>
+            <div  class="formComponent" id="content">
+                <div class="title">Content</div>
+                <input class="box">
+            </div>
         </div>
-        <div id="date">
-            <div>Date</div>
-            <button></button>  
-        </div>
-        <div id="importance">
-            <div>Importance</div>
-            <button></button>  
-        </div>
-        <div id="status">
-            <div>Status</div>
-            <button></button>  
-        </div>
-        <div id="content">
-            <div>Content</div>
-            <input>
-        </div>
+        <button>저장</button>
     </div>
 </template>
 <script>
@@ -30,4 +33,32 @@
         height: calc(100vh - 70px);
         padding: 20px 32px;
     }
+
+    .formComponent{
+        margin-top: 12px;
+        margin-bottom: 12px;
+    }
+    .title{
+        color: #F19F9D;
+        font-size: 14px;
+        font-family: 'Noto Sans', sans-serif;
+        font-weight: 500;
+        margin-bottom: 2px
+    }
+    .box{
+        border: 1px solid #7E869E;
+        width: 100%;
+        height: 52px;
+        border-radius: 22.5px;
+        /* padding: 12px 17px; */
+        background-color: transparent;
+        display: flex;
+        align-items: center; 
+        justify-content: flex-start; 
+        color: #7E869E;
+        font-family: 'Noto Sans', sans-serif;
+        font-weight: 500;
+        font-size: 14px;
+    }
+
 </style>
