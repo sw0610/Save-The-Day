5dd155aa637ca831aa7509c8f6ca4b9f4450b9ce
sw0610 <enthdnjs11@gmail.com>
2024-02-12 17:37:01 +0900

    feat: 로그인 구현 중

---
+++ Backend/src/main/java/com/savetheday/backend/dto/request/SignInMemberReq.java
@@ -0,0 +1,16 @@
+package com.savetheday.backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInMemberReq {
+
+    private String email;
+    private String password;
+}
